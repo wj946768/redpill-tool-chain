aa73270b9a68a082776e1e83f6026e40d1f22b89
jinlife <86603404+jinlife@users.noreply.github.com>
2022-06-03 19:21:41 +0800

    Update gen-key.sh

--- build/gen-key.sh
+++ build/gen-key.sh
@@ -11,7 +11,7 @@ echo "allow-loopback-pinentry" > ~/.gnupg/gpg-agent.conf
 
 echo RELOADAGENT | gpg-connect-agent
 
-gpg --batch --gen-key <<EOF
+gpg --gen-key <<EOF
 Key-Type: 1
 Key-Length: 2048
 Subkey-Type: 1
